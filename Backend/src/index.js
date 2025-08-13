70d5a45ef1ba7ff3c04fedb3ed626ceff03ace7e
saurabhsingh2026 <saurabh.singh@reddorz.com>
2025-08-13 14:39:22 +0530

    login functinality added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,11 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './db/index.js';
-
-<<<<<<< HEAD
-
-=======
->>>>>>> saurab_backend
+import userRoutes from './routes/user.routes.js'
 // Load environment variables
 dotenv.config();
 
@@ -24,12 +20,10 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.use("/api/v1/user",userRoutes )
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> saurab_backend
